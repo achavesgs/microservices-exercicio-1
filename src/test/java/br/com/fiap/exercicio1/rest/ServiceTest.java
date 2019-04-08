1080a1ab2069f071aa47a4d15efd6aa9843450dd
achavesgs <alinecgsoares@hotmail.com>
2019-04-08 13:58:25 -0300

    Update ServiceTest.java

--- src/test/java/br/com/fiap/exercicio1/rest/ServiceTest.java
+++ src/test/java/br/com/fiap/exercicio1/rest/ServiceTest.java
@@ -1,48 +1,73 @@
 package br.com.fiap.exercicio1.rest;
-
+ 
 import br.com.fiap.exercicio1.dto.OrderDTO;
 import br.com.fiap.exercicio1.repository.OrderRepository;
+ 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-
-@RunWith(SpringRunner.class)
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+ 
+ 
+@RunWith(MockitoJUnitRunner.class)
 @WebMvcTest(OrderRepository.class)
 public class ServiceTest {
-
-    @Autowired
-    private MockMvc mvc;
-    @MockBean
-    private Service service;
-
+   
+    @Mock
+      private OrderRepository repository;
+    
+    @InjectMocks
+      private Service service = new Service();
+ 
     @Test
     public void findById() {
-        OrderDTO orderDTO = mock(OrderDTO.class);
-
+        OrderDTO orderDTO = new OrderDTO();
+ 
         orderDTO.setIdPedido(1);
-
+ 
         service.findById(orderDTO.getIdPedido());
     }
-
+ 
     @Test
     public void save() {
+        OrderDTO orderDTO = new OrderDTO();
+ 
+        orderDTO.setIdPedido(1);
+        orderDTO.setBandeira("Visa");
+        orderDTO.setDsItemPedido("Teste");
+        orderDTO.setEmail("teste@teste.com.br");
+        orderDTO.setDtPedido("05/04/2019");
+        orderDTO.setFormaPagto("Debito");
+        orderDTO.setIdTransacao("1");
+        orderDTO.setNomeCompleto("Aline Chaves");
+        orderDTO.setNroCartao("1234567890");
+        orderDTO.setQtdItemPedido("10");
+ 
+        service.save(orderDTO);
     }
-
+ 
     @Test
     public void update() {
+        OrderDTO orderDTO = new OrderDTO();
+ 
+          orderDTO.setIdPedido(0);
+          orderDTO.setBandeira("Visa");
+          orderDTO.setDsItemPedido("Teste");
+          orderDTO.setEmail("teste@teste.com.br");
+          orderDTO.setDtPedido("05/04/2019");
+          orderDTO.setFormaPagto("Debito");
+          orderDTO.setIdTransacao("1");
+          orderDTO.setNomeCompleto("Aline Chaves");
+          orderDTO.setNroCartao("1234567890");
+          orderDTO.setQtdItemPedido("10");
+ 
+          service.update(orderDTO);
     }
-
+ 
     @Test
     public void delete() {
+      service.delete(1);
     }
 }
