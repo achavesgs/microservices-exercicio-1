e15ef09ed2676d345e08465894507019396a3e01
achaves <achaves@achaves-VirtualBox>
2019-04-14 15:50:08 -0300

    ajustes testes unitarios

--- src/test/java/br/com/fiap/exercicio1/rest/ServiceTest.java
+++ src/test/java/br/com/fiap/exercicio1/rest/ServiceTest.java
@@ -5,27 +5,52 @@ import br.com.fiap.exercicio1.repository.OrderRepository;
  
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import static org.hamcrest.Matchers.containsString;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import static org.mockito.Mockito.when;
  
  
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(SpringRunner.class)
 @WebMvcTest(OrderRepository.class)
 public class ServiceTest {
    
-    @Mock
+	@Autowired
+	private MockMvc mvc;
+	
+	@MockBean
       private OrderRepository repository;
     
     @InjectMocks
       private Service service = new Service();
  
     @Test
+    public void notFoundOrder() throws Exception {
+        int id = 1;
+        mvc.perform(get("/findById/"+id)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound()).andExpect(content().string(containsString("Pedido não encontrado")));
+    }
+    
+    @Test
     public void findById() {
         OrderDTO orderDTO = new OrderDTO();
  
         orderDTO.setIdPedido(1);
+        
+        when(this.service.findById(orderDTO.getIdPedido())).thenReturn(new ResponseEntity<OrderDTO>(orderDTO, HttpStatus.valueOf(200)));
  
         service.findById(orderDTO.getIdPedido());
     }
